a27c3ef1ca79defca20e494ee658c303bcd4ee8a
DevJpedroASeco <dev.jpedro@gmail.com>
2022-06-27 11:34:30 -0300

    Testa

--- src/Router.tsx
+++ src/Router.tsx
@@ -5,7 +5,7 @@ import { Subscribe } from "./pages/Subscribe"
 export function Router () {
   return (
     <Routes>
-      <Route path="/home" element={<Subscribe />} />
+      <Route path="/" element={<Subscribe />} />
       <Route path="/event" element={<Event />} />
       <Route path="/event/lesson/:slug" element={<Event />} />
     </Routes>
